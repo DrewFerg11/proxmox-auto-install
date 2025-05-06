d5cc29dce3850d0c71fe2ac5337f6fd117c2b245
Drew <dmferg5@gmail.com>
2025-05-05 21:27:05 -0400

    adding PBS stupport

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Proxmos Automatic Installation
+# Proxmox Automatic Installation
 
-Create  an installation `.iso` that can automatically install and perform the basic configure of a proxmox instance. 
+Create an installation `.iso` that can automatically install and perform the basic configure of a proxmox virtual environment (PVE) or proxmox backup server (PBS) instance.
 
 ## Description
 
-These steps are meant to setup the base instance of a proxmox instance. To further configure the instance, I recommend using ansible. 
+These steps are meant to setup the base instance of a proxmox instance. To further configure the instance, it is recommended to use ansible. 
 
 More information about the automatic installation process and configuration options can be found in the [official documentation](https://pve.proxmox.com/wiki/Automated_Installation) from proxmox.
 
@@ -12,8 +12,12 @@ More information about the automatic installation process and configuration opti
 
 You'll need to manually do the following:
 
-1. Download the latest version of the [
-Proxmox VE ISO Installer](https://www.proxmox.com/en/downloads) (currently 8.4-1) and place it in the root directory of this repo.
+1. Download the latest version of the installer `.iso` and place it in the root directory of this repo.
+    
+    - [Proxmox VE ISO Installer](https://www.proxmox.com/en/downloads/proxmox-virtual-environment) (currently 8.4-1) 
+    - [Proxmox Backup Server ISO Installer](https://www.proxmox.com/en/downloads/proxmox-backup-server) (currently 3.4-1) 
+
+    Example using Proxmox VE, version 8.2-2
 
     ![alt text](./resources/screenshot01.png)
 
@@ -24,7 +28,13 @@ Proxmox VE ISO Installer](https://www.proxmox.com/en/downloads) (currently 8.4-1
 
 ## Executing
 
-1. Execute the `create-iso.sh` script and pass in the hostname of the server you want to create the `.iso` for. Example: `./create-iso.sh pve01`
+1. Execute the `create-iso.sh` script and pass in:
+
+    - The proxmox type, `pve` or `pbs`
+    - The hostname of the server you want to create the `.iso` for.
+
+    Example: `./create-iso.sh pve pve01`
+
 2. Burn the resulting `.iso` to a usb drive using balenaEtcher (or a similar tool).
 
     ![alt text](./resources/screenshot02.png)
--- create-iso.sh
+++ create-iso.sh
@@ -1,29 +1,57 @@
 # !/bin/bash
 
-# Verify the hostname arguement was passed.  
-if [ "$#" -ne 1 ]; then
-    printf "*** Invalid arguements provided ***\n\n"
-    printf "Excute script using: ./create-iso.sh hostname\n\n"
+# Verify the hostname argument was passed.  
+if [ "$#" -ne 2 ]; then
+    printf "*** Invalid arguments provided ***\n\n"
+    printf "Execute script using: ./create-iso.sh proxmox_type  hostname\n\n"
     exit 1
 fi
 
-iso_hostname=${1}
-proxmox_version=8.4-1
+# Increment the version number for new releases.
+PVE_VERSION=8.4-1
+PBS_VERSION=3.4-1
+PVE_BASE="proxmox-ve"
+PBS_BASE="proxmox-backup-server"
+
+# Passed arguments.
+proxmox_type=${1}
+iso_hostname=${2}
+
+# Verify the proxmox_type argument was passed.
+if [ "$proxmox_type" != "pbs" ] && [ "$proxmox_type" != "pve" ]; then
+    echo "Invalid proxmox_type: $proxmox_type"
+    exit 1
+fi
+
+# Final proxmox version and base name.
+proxmox_version=${PVE_VERSION}
+proxmox_base=${PVE_BASE}
+if [ "$proxmox_type" == "pbs" ]; then
+    proxmox_version=${PBS_VERSION}
+    proxmox_base=${PBS_BASE}
+fi
+
+# Verify the hostname answer file exists.
+if [ ! -f ./answers/answer-"${iso_hostname}".toml ]; then
+    printf "*** Missing answer file ***\n\n"
+    printf "Expecting answer file for hostname ${iso_hostname}.\n\n"
+    exit 1
+fi
 
 # Verify the base proxmox installation iso exists.
-if [ ! -f proxmox-ve_"${proxmox_version}".iso ]; then
+if [ ! -f "${proxmox_base}"_"${proxmox_version}".iso ]; then
     printf "*** Missing proxmox .iso ***\n\n"
-    printf "Expecting proxmox installation .iso for version ${proxmox_version}.\n\n"
+    printf "Expecting proxmox ${proxmox_type} installation .iso for version ${proxmox_version}.\n\n"
     exit 1
 fi
 
-printf "\nCreating automatic installation .iso for ${iso_hostname} and proxmox version ${proxmox_version}.\n\n"
+printf "\nCreating automatic installation .iso for ${iso_hostname} and proxmox ${proxmox_type} version ${proxmox_version}.\n\n"
 
 # Create the auto install .iso file.
-proxmox-auto-install-assistant prepare-iso ./proxmox-ve_"${proxmox_version}".iso --fetch-from iso --answer-file ./answers/answer-"${iso_hostname}".toml
+proxmox-auto-install-assistant prepare-iso ./"${proxmox_base}"_"${proxmox_version}".iso --fetch-from iso --answer-file ./answers/answer-"${iso_hostname}".toml
 
 # Rename the .iso file.
-cp ./proxmox-ve_"${proxmox_version}"-auto-from-iso.iso ./proxmox-ve_"${proxmox_version}"-auto-from-iso-"${iso_hostname}".iso
-rm -f ./proxmox-ve_"${proxmox_version}"-auto-from-iso.iso 
+cp ./"${proxmox_base}"_"${proxmox_version}"-auto-from-iso.iso ./"${proxmox_base}"_"${proxmox_version}"-auto-from-iso-"${iso_hostname}".iso
+rm -f ./"${proxmox_base}"_"${proxmox_version}"-auto-from-iso.iso 
 
 printf "\nDone...\n"
